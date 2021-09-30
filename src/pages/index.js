bcff7d78c881e1f01e470122fbd40e5412300a57
Natalia <dnataliacr@gmail.com>
2021-09-29 23:24:44 -0300

    add home component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import Layout from "../components/layout/Layout"
 
-
+import Home from "../components/home/content/Home"
 // markup
 const IndexPage = () => {
   return (
-<Layout></Layout>
+<Layout>
+  <Home/>
+</Layout>
   )
 }
 
