2c9f012c7cf9dcd0baf74552051a2d3cd97614ca
Natalia <dnataliacr@gmail.com>
2021-09-29 23:36:03 -0300

    fix routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import Layout from "../components/layout/Layout"
 
-import Home from "../components/home/content/Home"
+import Home from "../components/home/Home"
 // markup
 const IndexPage = () => {
   return (
