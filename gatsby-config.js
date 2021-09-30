162abfb171abe0231adb29845ac0060fd5d4e6e7
Natalia <dnataliacr@gmail.com>
2021-09-29 21:48:06 -0300

    install plugins & dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,26 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "sacu",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        // Footnotes mode (default: true)
+        footnotes: true,
+        // GitHub Flavored Markdown mode (default: true)
+        gfm: true,
+        // Plugins configs
+        plugins: [],
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
