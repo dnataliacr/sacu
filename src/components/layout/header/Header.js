2de10290daf58729a8e429d35c78d5469b68ddf8
Natalia <dnataliacr@gmail.com>
2021-09-29 22:09:54 -0300

    add header components

---
+++ src/components/layout/header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Logo from './logo/Logo';
+import Nav from './navBar/Nav'
+const Header = () => {
+    return (
+        <>
+        <Logo/>
+        <Nav/>
+        </>
+    )
+}
+
+
+export default Header
