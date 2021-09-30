e2aa771481529f38becc51a757dca5bd18f2b18c
Natalia <dnataliacr@gmail.com>
2021-09-29 23:24:07 -0300

    import components

---
+++ src/components/home/content/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import SliderBanner from "../sliderBanner/SliderBanner";
+
+const Home = () => {
+  return ( 
+  <>
+  <SliderBanner />
+
+  </>
+  )
+};
+
+export default Home;
