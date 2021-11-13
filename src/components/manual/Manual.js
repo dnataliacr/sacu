05783e0fd2a93e3d69d1a46887cc047649047ecb
Natalia <dnataliacr@gmail.com>
2021-11-12 22:13:21 -0300

    Create Manual.js

---
+++ src/components/manual/Manual.js
@@ -0,0 +1,235 @@
+import React from "react";
+
+const Manual = () => {
+  return (
+    <div>
+      <div>
+        <h1> Pequeño manual de incubación</h1>
+        <div>
+          <p>
+            <br> Incubación:</br>
+            Describiremos la incubación como una etapa más dentro del proceso
+            reproductivo de las aves, más concretamente en la gallina. Antes de
+            comenzar a describír la incubación debemos definir que es un huevo
+            fértil: es aquel producido por un plantel reproductor en el que
+            conviven machos y hembras y se encuentra fertilizado, cuando este es
+            puesto (20hr) ya existe desarrollo embrionario en él (blastodermo)
+          </p>
+        </div>
+        <div>
+          <p>
+            _____________________________________________________________________
+            Huevo incubable es aquel huevo fecundado, conservado y transportado
+            hasta la incubación en condiciones adecuadas de manejo e higiene y
+            con una calidad externa (cascará) e interna (albumen) que permita el
+            desarrollo embrionario.
+          </p>
+        </div>
+        <div>
+          <p>
+            Fenómenos importantes del desarrollo embrionario: Previamente de la
+            postura: Fecundación Entre postura e incubación: se detiene el
+            desarrollo, estado de vida latente del embrión.
+          </p>
+        </div>
+
+        <div>
+          <h2> Incubación natural: </h2>
+          <p>
+            La incubación abarca en las aves la segunda etapa de los procesos
+            reproductivos, esta viene acompañada con cambios en el
+            comportamiento de las mismas, apareciendo el instinto maternal que
+            regula la incubación de los huevos y cuidado de las crías. El
+            estímulo incubador cloquez, con la aparición de la incubación
+            artificial no solo no es deseable, sino que desde el punto de vista
+            productivo es perjudicial, ya que reduce el número de huevos puestos
+            por hembra.
+          </p>
+          <em>
+            Por ese motivo tratamos de anular o disminuir ese instinto,
+            utilizando dos métodos complementarios:{" "}
+          </em>
+          <h3>
+            {" "}
+            Modificación de factores medioambientales que favorecen la aparición
+            de la cloquez.
+          </h3>
+          <ul>
+            <li>
+              Nido: la existencia de ellos hace que la hembra pase mucho tiempo
+              en ellos estimulando la cloquez.
+            </li>
+
+            <li>
+              Presencia de huevo en los nidos, de aquí la importancia de
+              recogida frecuentes a lo largo del día.
+            </li>
+
+            <li>
+              Temperatura ambiental al aumentar favorece el cese de la puesta y
+              el inicio de la incubación.
+            </li>
+          </ul>
+
+          <h3>2) Selección genética de las aves: </h3>
+
+          <p>
+            {" "}
+            Aquí tenemos que en las líneas de ponedoras comerciales una
+            tendencia muy baja a la cloquez, lo mismo ocurre con ciertas razas
+            usadas en la avicultura clásica que por la selección que ha sido
+            efectuada durante años es prácticamente inexistente la aparición de
+            la cloquez, por ejemplo en la raza New Hampshire. Lo mismo ocurre
+            con el caso de la codorniz, no así con otras aves en donde la
+            tendencia a la cloquez es muy acentuada como por ejemplo ganso,
+            pato, pavo.
+          </p>
+          <p>
+            En el manejo de las aves previniendo la cloquez, además de las
+            medidas anteriores, debemos identificar rápidamente las hembras
+            cluecas por sus cambios morfológicos, disminución de peso,
+            disminución del tamaño de la cresta, áreas sin plumas en el abdomen,
+            así como cambios en la conducta, permanencia en los nidos,
+            agresividad. Si esto ocurre debemos trasladar la hembra para
+            modificar su ambiente colocándola en jaula y favorecer el consumo de
+            alimento.
+          </p>
+        </div>
+
+        <div>
+          <h2>Incubación artificial: </h2>
+
+          <p>
+            Primeramente trataremos el manejo del huevo fértil, ya que este una
+            vez puesto por la gallina detiene el desarrollo siempre y cuando la
+            temperatura sea menor a 21C, de ahí la importancia de un correcto
+            manejo y almacenamiento del mismo hasta el momento de cargar la
+            incubadora.
+            <h3>El galpón o granja: </h3>
+            <br> Nidos: </br>
+            correcta cantidad de los mismos según el número de aves, uno por
+            cada cuatro gallinas, en caso de ponederos comunitarios 1m2/25, con
+            material limpio e higiénico, cambiado regularmente. Recuerde que
+            cuando el huevo es puesto se encuentra limpio y libre de
+            microorganismos.
+            <br> Recolección:</br>a intervalos frecuentes 4/5 día, que puede ser
+            necesario aumentar en días calurosos o climas cálidos.
+            <br>
+              Eliminación para la incubación de los huevos recogidos del piso,
+            </br>
+            ya que estos entraron en contacto con la cama y materias fecales por
+            lo cual poseen una alta contaminación.
+            <br> Eliminación de huevos sucios, </br>
+            sobre todo si es con materia
+            fecal 
+            <br> Desinfección: </br> 
+            - Rociado con desinfectante: Rociado de los
+            huevos con Formol l 1% amonios cuaternarios (cloruro de
+            benzalconio). 
+            
+            <h5> Luego estamos en condiciones de seleccionarlos y
+            almacenarlos hasta el momento de cargar la incubadora.</h5>
+
+            <ul> </ul>
+            -Almacenamiento: Cuando la temperatura ambiente es alta (27) hay que
+            trasladarlos rápidamente a un lugar con temperatura por debajo de
+            los 20C para así evitar el desarrollo del embrión que luego puede
+            debilitarse y morir en la incubación, por este motivo deben evitarse
+            las fluctuaciones de temperatura. -Humedad: Entre 70-80% no debiendo
+            alcanzar la saturacion, ya que la condensación de agua sobre la
+            cáscara del huevo proporciona un medio para la multiplicación
+            bacteriana. - Temperatura: Depende del tiempo previsto de
+            conservación, si no sabemos cuantos días va a estar almacenado
+            recomendamos conservaros a 15-18C, las líneas de huevo castaño
+            necesitaran un poco más de H: Blanco 12-15 c-H: Castaño 18C.
+            -Tiempo: El óptimo de almacenamiento son tres día, pero puede llegar
+            a siete. Deben mantenerse en maples siempre con la cámara de aire
+            (extremo romo) hacia arriba TABLITA Selección: Tratamos en este
+            momento de identificar los huevos no aptos para la incubación, es un
+            paso previo al cargado de la incubadora. No aptos: Demasiado grandes
+            o pequeños, dependiendo de la línea 52-64gr. Doble yema Sucios/rotos
+            Problemas de cáscara débil o gruesa Cámara de aire grande o flotante
+            Alteración de la forma, anillada Presencia de manchas de sangre.
+            Segunda desinfección: La realizaremos con los mismos productos que
+            vimos para la desinfección en la granja. La importancia de la
+            desinfección la podemos observar el siguiente cuadro: CUADRITO TABLA
+            INCUBACIÓN: El proceso de incubación depende esencialmente de cuatro
+            factores: Temperatura Humead Ventilación Volteo Las condiciones de
+            incubación ideales son: Temperatura: 37.8 Humedad: 60% Ventilación:
+            Dióxido de Carbono 0.5% Oxigeno 21% Volteo máximo cada una hora. En
+            cuanto a incubadoras caseras en las cuales no podemos regular la
+            ventilación se aconseja a partir de la semana cuando hacemos los
+            volteos dejar unos minutos abierta la puerta para favorecer la
+            ventilación. El volteo es necesario para evitar que el embrión se
+            adhiera a la cara interna de a cáscara y membranas vitelinas. El
+            ángulo correcto de volteo es 45 a cada lado de la vertical (total
+            90) en cuanto a la frecuencia de los sistemas actuales programan
+            volteos cada hora, en máquinas manuales lo recordable sería 6
+            volteos diarios fundamentalmente la primera semana que es cuando el
+            riesgo de adherencia es mayor. Recordar que debemos colocarlos en la
+            incubadora siempre con la cara de aire hacia arriba.
+          </p>
+        </div>
+        <div className="tab">
+          Fenómenos importantes en el desarrollo embrionario durante la
+          incubación: Di TABLA 1 Comienza a esbozarse el embrión 2 Comienza a
+          latir el corazón 3 Formación de amnios y alantoides 4 Esbozo de cola,
+          patas y ala. 5 Formación de los órganos de la reproducción y
+          diferenciación sexual 8 Comienza la formacion de las plumas 10
+          comienza la mineralización ósea 14 Gira la cabeza hacia el polo obtuso
+          del huevo 17 Vuelve el pico hacia la cámara de aire 19 Empieza a
+          reabsorberse el saco vitelino 20 El saco vitelino está dentro del
+          cuerpo, el embrión ocupa todo el interior del huevo excepto la cámara
+          de aire.
+        </div>
+
+        <div>
+          {" "}
+          <p>
+            TRANSFERENCIA: En el ámbito de la incubación artificial se llama
+            transferencia al proceso de trasladar los huevos que se han incubado
+            18 días en el caso de la gallina, a una bandeja en la que se
+            producirá el nacimiento, para realizarlo correctamente es necesario
+            que el embrión no sufra bajas temperaturas y no dañar con golpes el
+            huevo durante el procedimiento. En las incubadoras horizontales se
+            puede instalar una bandeja en la parte más baja de la incubadora
+            para producir en ella el nacimiento de los bebes. Los motivos por
+            los cuales es necesario separar las actividades de incubación y
+            nacimiento son las siguientes: Productivo: En nacedoras se mejora el
+            índice de eclosión y viabilidad del pollito por disminución de la
+            temperatura y aumento de la humedad. Sanitario: El nacimiento es un
+            momento de gran diseminación bacteriana que puede contaminar los
+            huevos que están siendo incubadas.
+          </p>
+        </div>
+
+        <div>
+          <p>
+            Las condiciones ideales de temperatura nacedera son: TABLA
+            Temperatura: 36.8C Humedad: 70% Ventilación: Dióxido de Carbono 0.5%
+            Oxigeno 21% No se ejecuta volteo Control de incubación: El primer
+            miraje lo llevaremos a cabo al 5-6 día para eliminar los huevos no
+            fecundados, aquí los distinguimos como “huevos claros”, el miraje
+            debe ser realizado lo mas rápido posible para evitar el enfriamiento
+            de los huevos. El 2 miraje lo practicamos en el momento de pasaje a
+            nacedera (día 18). El tiempo de eclosión puede variar en 24 horas
+            Los factores que incrementan el tiempo de incubación son: Huevos
+            viejos, con largo periodo de almacenamiento, por cada día aumenta en
+            una hora el tiempo de incubación. Baja temperatura en la incubadora
+            Huevos de gran tamaño Los huevos almacenados por más de siete días
+            deben ser cargados 30 minutos antes por cada día que pase del
+            octavo. Los factores que disminuyen el tiempo de incubación son:
+            Alta temperatura en la incubadora Huevos pequeños Huevos de lote
+            reproductor de animales jóvenes Por último que tomar en cuenta el
+            número de pollitos nacidos por el total de huevos cargados en la
+            incubadora es el índice que llamaremos incurabilidad. Incurabilidad
+            = N BB nacidos x 100 _____________________________________ Huevos
+            cargados
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Manual;
